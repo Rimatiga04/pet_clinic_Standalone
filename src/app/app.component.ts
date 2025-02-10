5153165f886b209484d3ccaa0de069e5ac7ad8c4
Ricardo <rimatiga04@gmail.com>
2025-02-10 14:45:58 +0100

    Añadido la barra de navegacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,57 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-
+import { RouterOutlet, Router } from '@angular/router';
+import { MenubarDesignTokens } from '@primeng/themes/types/menubar';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenubarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'pet_clinic_Standalone';
+  menuOptions: MenuItem[] | undefined;
+  ngOnInit() {
+    this.menuOptions = [
+      {
+        label: 'Owners',
+        icon: 'pi pi-user',
+        items: [
+          {
+            label: 'Todos',
+            icon: 'pi pi-users',
+            routerLink: '/'
+          },
+          {
+            label: 'Añadir Owner',
+            icon: 'pi pi-user-plus',
+            routerLink: 'owner-add/:id'
+          }
+        ]
+      },
+      {
+        label: 'Veterinarios',
+        icon: 'pi pi-briefcase',
+        items: [
+          {
+            label: 'Ver Todos',
+            icon: 'pi pi-briefcase',
+            routerLink: 'vets'
+          },
+          {
+            label: 'Add Vet',
+            icon: 'pi pi-plus-circle',
+            routerLink: 'vets-add/:id'
+          }
+        ]
+      },
+      {
+        label: 'Pet Types',
+        icon: 'pi pi-heart',
+        routerLink: 'petTypes'
+      }
+    ]
+  }
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,9 @@ import { Routes } from '@angular/router';
 import { OwnersComponent } from './components/owners/owners.component';
 import { FormOwnerComponent } from './components/form-owner/form-owner.component';
 import { DetailOwnerComponent } from './components/detail-owner/detail-owner.component';
+import { VetsComponent } from './components/vets/vets.component';
+import { VetAddComponent } from './components/vet-add/vet-add.component';
+import { PettypeListComponent } from './components/pettype-list/pettype-list.component';
 
 export const routes: Routes = [
   {
@@ -15,5 +18,17 @@ export const routes: Routes = [
   {
     path: "owner-detail/:id",
     component: DetailOwnerComponent
+  },
+  {
+    path: "vets",
+    component: VetsComponent
+  },
+  {
+    path: "vets-add/:id",
+    component: VetAddComponent
+  },
+  {
+    path: "petTypes",
+    component: PettypeListComponent
   }
 ];
