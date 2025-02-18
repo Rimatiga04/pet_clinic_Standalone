7cf8667e8ecd47ea088e51d89e2a4e3141664eab
Ricardo <rimatiga04@gmail.com>
2025-02-18 14:50:09 +0100

    añadimos un par de cositas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,57 @@ import { RouterOutlet, Router } from '@angular/router';
 import { MenubarDesignTokens } from '@primeng/themes/types/menubar';
 import { MenuItem } from 'primeng/api';
 import { MenubarModule } from 'primeng/menubar';
+import { SelectItemGroup } from 'primeng/api';
+import { SelectModule } from 'primeng/select';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenubarModule],
+  imports: [RouterOutlet, MenubarModule, FormsModule, SelectModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'pet_clinic_Standalone';
   menuOptions: MenuItem[] | undefined;
+  groupedCities: SelectItemGroup[];
+
+  selectedCity: string | undefined;
+
+  constructor() {
+    this.groupedCities = [
+      {
+        label: 'Germany',
+        value: 'de',
+        items: [
+          { label: 'Berlin', value: 'Berlin' },
+          { label: 'Frankfurt', value: 'Frankfurt' },
+          { label: 'Hamburg', value: 'Hamburg' },
+          { label: 'Munich', value: 'Munich' }
+        ]
+      },
+      {
+        label: 'USA',
+        value: 'us',
+        items: [
+          { label: 'Chicago', value: 'Chicago' },
+          { label: 'Los Angeles', value: 'Los Angeles' },
+          { label: 'New York', value: 'New York' },
+          { label: 'San Francisco', value: 'San Francisco' }
+        ]
+      },
+      {
+        label: 'Japan',
+        value: 'jp',
+        items: [
+          { label: 'Kyoto', value: 'Kyoto' },
+          { label: 'Osaka', value: 'Osaka' },
+          { label: 'Tokyo', value: 'Tokyo' },
+          { label: 'Yokohama', value: 'Yokohama' }
+        ]
+      }
+    ];
+  }
   ngOnInit() {
     this.menuOptions = [
       {
--- src/app/services/pet.service.ts
+++ src/app/services/pet.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { Pet } from '../models/pet';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PetService {
+  private url = environment.API_URL;
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  AnadePet(pet: Pet){
+    let cuerpo = {
+      accion: "AnadePet",
+      pet: pet
+    }
+    return this.http.post<Pet[]>(this.url, cuerpo);
+  }
 }
